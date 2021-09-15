2d0b67aeb26167053cab9585c6dcbe3666f67539
averylargelake <timoroope.tuuri@gmail.com>
2021-09-15 03:07:02 +0300

    part1 exercise unicafe

--- part1/Exercises/unicafe/src/App.js
+++ part1/Exercises/unicafe/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = (props) => {
+  
+  if (props.sum===0 ) {
+    return (
+      <tr>
+        <td>No data</td>
+      </tr>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.stats}</td>
+    </tr>
+  )
+
 }
 
-export default App;
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={increaseGood} text='Good'/>
+      <Button handleClick={increaseNeutral} text='Neutral'/>
+      <Button handleClick={increaseBad} text='Bad'/>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <Statistics name ='Good' sum = {good + bad +neutral} stats={good}/>
+          <Statistics name = 'Neutral' sum = {good + bad +neutral} stats={neutral}/>
+          <Statistics name = 'Bad' sum = {good + bad +neutral} stats={bad}/>
+          <Statistics name = 'All' sum = {good + bad +neutral} stats={bad + good + neutral}/>
+          <Statistics name = 'Average' sum = {good + bad +neutral} stats={(good - bad) / (bad + good + neutral) }/>
+          <Statistics name = 'Positive' sum = {good + bad +neutral} stats={(good / (bad + good + neutral))* 100}/>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
--- part1/random_code_along/1c.js
+++ part1/random_code_along/1c.js
@@ -28,7 +28,7 @@ const App = () => {
 }
 /* Equivalent
 const bornYear = () => new Date().getFullYear() - age
-
+THESE ARE THE SAME
 const bornYear = () => {
   return new Date().getFullYear() - age
 }
@@ -69,3 +69,50 @@ const App = () => {
   )
 }
 */
+
+/* reusable button compontent
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+*/
+/*Event handler
+<button onClick={() => setValue(0)}>button</button>
+*/
+/*fuction declare shit
+const hello = (who) => {
+  const handler = () => {
+    console.log('hello', who)
+  }
+
+  return handler
+}
+IS THE SAME AS -->
+const hello = (who) => {
+  return () => {
+    console.log('hello', who)
+  }
+}
+BECAUSE SINGLE RETURN STATEMENT
+const hello = (who) =>
+  () => {
+    console.log('hello', who)
+  }
+more neatly
+const hello = (who) => () => {
+  console.log('hello', who)
+}
+*/
+<Button
+        onClick={increaseByOne}
+        text='plus'
+      />
